65dcaefee9cb4c1acdf1a5263d34a19d35f539e1
kaizz04 <kaveesh@marketpushapps.com>
2024-09-05 12:43:29 +0530

    add checking wining condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const Monster_ATTACK_VALUE=14;
 let chosenMaxLife = 100;
 
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,19 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler(){
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
+
+    const playerDamage = dealPlayerDamage(Monster_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if(currentMonsterHealth <=0 && currentPlayerHealth > 0){
+        alert('You Won!')
+    }else if(currentPlayerHealth <=0 && currentMonsterHealth > 0){
+        alert('Monster Won!');
+
+    }else if(currentPlayerHealth <=0 && currentMonsterHealth <=0){
+        alert('You Have a draw!');
+
+    }
 }
 
 attackBtn.addEventListener('click',attackHandler);
