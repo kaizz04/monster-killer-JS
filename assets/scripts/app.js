1e1747199c7c4efb2f8e95ae5f6693b8df44a7f0
kaizz04 <kaveesh@marketpushapps.com>
2024-09-06 08:42:19 +0530

    added bonus life function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,14 +7,23 @@ let chosenMaxLife = 100;
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound(){
-    
+
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(Monster_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
 
+    if(currentPlayerHealth<=0 && hasBonusLife === true){
+        hasBonusLife=false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        alert("Your Bonus life save You")
+        setPlayerHealth(initialPlayerHealth);
+    }
     if(currentMonsterHealth <=0 && currentPlayerHealth > 0){
         alert('You Won!')
     }else if(currentPlayerHealth <=0 && currentMonsterHealth > 0){
