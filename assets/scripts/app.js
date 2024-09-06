9e12d8180a8965afd9c4002f953f1f65703f416f
kaizz04 <kaveesh@marketpushapps.com>
2024-09-06 11:29:39 +0530

    optimize code with switch case and ternary expression

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -34,45 +34,90 @@ function writeLog(ev,val,monsterHealth,playerHealth){
         finalMonsterHealth:monsterHealth,
         finalPlayerHealth:playerHealth
     };
-    if(ev === LOG_EVENT_PLAYER_ATTACK){
-        logEntry.target ='Monster';
-   
-    }else if(ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
-        logEntry = {
-            event:ev,
-            value:val,
-            target:'Monster',
-            finalMonsterHealth:monsterHealth,
-            finalPlayerHealth:playerHealth
-        };
-       
-    }else if(ev === LOG_EVENT_MONSTER_ATTACK){
-        logEntry = {
-            event:ev,
-            value:val,
-            target:'Monster',
-            finalMonsterHealth:monsterHealth,
-            finalPlayerHealth:playerHealth
-        };
-       
-    }else if(ev === LOG_EVENT_PLAYER_HEAL){
-        logEntry = {
-            event:ev,
-            value:val,
-            target:'Monster',
-            finalMonsterHealth:monsterHealth,
-            finalPlayerHealth:playerHealth
-        };
-       
-    }else if(ev === LOG_EVENT_GAME_OVER){
-        logEntry = {
-            event:ev,
-            value:val,
-            finalMonsterHealth:monsterHealth,
-            finalPlayerHealth:playerHealth
-        };
-       
+    switch(ev){
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'Monster';
+            break;
+            
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry = {
+                event:ev,
+                value:val,
+                target:'Monster',
+                finalMonsterHealth:monsterHealth,
+                finalPlayerHealth:playerHealth
+            };
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry = {
+                event:ev,
+                value:val,
+                target:'Monster',
+                finalMonsterHealth:monsterHealth,
+                finalPlayerHealth:playerHealth
+            };
+            break;
+
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry = {
+                event:ev,
+                value:val,
+                target:'Monster',
+                finalMonsterHealth:monsterHealth,
+                finalPlayerHealth:playerHealth
+            
+            };
+            break;
+
+        case LOG_EVENT_GAME_OVER:
+            logEntry = {
+                event:ev,
+                value:val,
+                finalMonsterHealth:monsterHealth,
+                finalPlayerHealth:playerHealth
+            };
+            break;
+
     }
+    // if(ev === LOG_EVENT_PLAYER_ATTACK){
+    //     logEntry.target ='Monster';
+   
+    // }else if(ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
+    //     logEntry = {
+    //         event:ev,
+    //         value:val,
+    //         target:'Monster',
+    //         finalMonsterHealth:monsterHealth,
+    //         finalPlayerHealth:playerHealth
+    //     };
+       
+    // }else if(ev === LOG_EVENT_MONSTER_ATTACK){
+    //     logEntry = {
+    //         event:ev,
+    //         value:val,
+    //         target:'Monster',
+    //         finalMonsterHealth:monsterHealth,
+    //         finalPlayerHealth:playerHealth
+    //     };
+       
+    // }else if(ev === LOG_EVENT_PLAYER_HEAL){
+    //     logEntry = {
+    //         event:ev,
+    //         value:val,
+    //         target:'Monster',
+    //         finalMonsterHealth:monsterHealth,
+    //         finalPlayerHealth:playerHealth
+    //     };
+       
+    // }else if(ev === LOG_EVENT_GAME_OVER){
+    //     logEntry = {
+    //         event:ev,
+    //         value:val,
+    //         finalMonsterHealth:monsterHealth,
+    //         finalPlayerHealth:playerHealth
+    //     };
+       
+    // }
     battleLog.push(logEntry);
 
 }
@@ -121,18 +166,22 @@ function endRound(){
 }
 
 function attackMonster(mode){
-    let maxDamage;
-    let logEvent;
-    if(mode === MODE_ATTACK){
-        maxDamage = ATTACK_VALUE;
-        let logEvent=LOG_EVENT_PLAYER_ATTACK;
+    const maxDamage = mode ===MODE_ATTACK 
+    ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+
+    const logEvent = mode === MODE_ATTACK 
+    ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
+
+    // if(mode === MODE_ATTACK){
+    //     maxDamage = ATTACK_VALUE;
+    //     let logEvent=LOG_EVENT_PLAYER_ATTACK;
         
 
-    }else if(mode === MODE_STRONG_ATTACK){
-        maxDamage = STRONG_ATTACK_VALUE;
-        let logEvent=LOG_EVENT_PLAYER_STRONG_ATTACK;
+    // }else if(mode === MODE_STRONG_ATTACK){
+    //     maxDamage = STRONG_ATTACK_VALUE;
+    //     let logEvent=LOG_EVENT_PLAYER_STRONG_ATTACK;
 
-    }
+    // }
 
 
     const damage = dealMonsterDamage(maxDamage);
